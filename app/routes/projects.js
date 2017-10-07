f6eb31a677ceb004a7656a9b5d81576ba6daeb29
bargav.kondapu <bargav.kondapu@karsun-llc.com>
2017-10-07 19:46:19 -0400

    KBS-1: Projects CSS grid, layout.

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     return {
-      data: [{ id:'p1', type:"project", attributes: {image:"project1.jpg", description:"project 1 description"}},
-        { id:"p2",type:"project", attributes: {image: "project2.jpg", description: "project 2 description"}}]
+      data: [{ id:'1', title:'Drupal Migrate Taxonomy To Groups', slug:'drupal-migrate-taxonomy-to-groups', weight:'', category:"module", attributes: {image:"project1.jpg", description:"project 1 description", url:""}},
+        { id:'2', title:'Haveli Bistro', slug:'Haveli Bistro', weight:'', category:"website", attributes: {image:"project2.jpg", description:"project 2 description", url:"www.havelibistro.com"}}]
     };
   }
 });
