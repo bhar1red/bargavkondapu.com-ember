bc32c738cc32964c472beb425f17a023aa2868dc
bargav.kondapu <bargav.kondapu@karsun-llc.com>
2017-10-15 16:36:22 -0400

    Data now points to actual site.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://bargavkondapu.dd:8083',
+  host: 'http://ec2-34-231-236-238.compute-1.amazonaws.com',
   namespace: 'api/v1'
 });
