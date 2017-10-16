f61340116e1f234d4966e8d86cda082aeac16e7c
bargav.kondapu <bargav.kondapu@karsun-llc.com>
2017-10-15 21:45:13 -0400

    Updated Homepage text.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    var text = "<p> I am Bargav and I am in a constant pursuit of fulfillment. I feel fulfilled by sharing knowledge and inspiration with others. I feel fulfilled by having deep conversations with people. </p> <p> Being grown up with technology, writing code and developing websites is one of the most fulfilling activities for me. I help organizations establish their digital presence and be able to share their thoughts across internet.</p>";
+    return text;
+  }
 });
