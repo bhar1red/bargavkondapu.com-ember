21eccd431469e52007723117ee011ad17d9bb5f1
bargav.kondapu <bargav.kondapu@karsun-llc.com>
2017-10-16 18:04:39 -0400

    fixed sort in projects.

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    var projects = this.store.findAll('project', {reload: true}).then(projects => projects.sortBy('id'));
+    var projects = this.store.findAll('project', {reload: true}).then(projects => projects);
     return projects;
   }
 });
